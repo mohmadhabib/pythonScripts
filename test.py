16970f7a18de4ead800dc1bcf565aeab40a6b76a
Mohammad Habib <mohmad.habib@gmail.com>
2022-02-15 00:29:50 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+import requests, user_agent, bs4
+
+
+r = requests.get("http://codelist.cc", headers={"User-Agent": str(user_agent.generate_user_agent())}
+s = bs4.BeautifulSoup(r.text, "lxml")
+print(s.title.text)
